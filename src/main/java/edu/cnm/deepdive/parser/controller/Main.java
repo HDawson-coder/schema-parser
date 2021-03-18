f524a06e83c86ddcd977f787b517a440c9f9ab75
Hana Dawson <stubbornunicorn95@gmail.com>
2021-03-18 10:02:49 -0600

    Completed parser with DDL output.

--- src/main/java/edu/cnm/deepdive/parser/controller/Main.java
+++ src/main/java/edu/cnm/deepdive/parser/controller/Main.java
@@ -2,12 +2,21 @@ package edu.cnm.deepdive.parser.controller;
 
 import edu.cnm.deepdive.parser.service.SchemaRepository;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Main {
 
+  private static final String STATEMENT_DELIMITER = ";\n\n";
+
+
   public static void main(String[] args) throws IOException {
     SchemaRepository repository = new SchemaRepository(System.in);
-    System.out.println("Read schema from standard in");
+    repository
+        .getSchema()
+        .ddlStream()
+        .map((statement) -> statement.concat(STATEMENT_DELIMITER))
+        .forEach(System.out::print);
   }
 
 }
+
--- src/main/java/edu/cnm/deepdive/parser/model/Index.java
+++ src/main/java/edu/cnm/deepdive/parser/model/Index.java
@@ -1,7 +1,59 @@
 package edu.cnm.deepdive.parser.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
 public class Index {
 
-  //TODO Add fields, accessors, mutators and @Expose annotations.
+  @Expose
+  private String name;
+
+  @Expose
+  private Boolean unique;
+
+  @Expose
+  private List<String> columnNames;
+
+  @Expose
+  @SerializedName("createSql")
+  private String sqlStatement;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Boolean getUnique() {
+    return unique;
+  }
+
+  public void setUnique(Boolean unique) {
+    this.unique = unique;
+  }
+
+  public List<String> getColumnNames() {
+    return columnNames;
+  }
+
+  public void setColumnNames(List<String> columnNames) {
+    this.columnNames = columnNames;
+  }
+
+  public String getSqlStatement() {
+    return sqlStatement;
+  }
+
+  public void setSqlStatement(String sqlStatement) {
+    this.sqlStatement = sqlStatement;
+  }
+
+  public String toDdl() {
+    return sqlStatement;
+
+  }
 
 }
